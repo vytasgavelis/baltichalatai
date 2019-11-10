a3f14b9210f8c813d7fa862ea5b12350f030c397
Diana V <d.valaityte.97@gmail.com>
2019-11-10 20:41:37 +0200

    Designed a search-results page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -27,4 +27,28 @@ class HomeController extends AbstractController
             'is_logged_in' => false,
         ]);
     }
+    /**
+     * @Route("/results", name="results")
+     */
+    public function results()
+    {
+        return $this->render('home/results.html.twig', [
+            'search_results' => [ 
+                'specialist' => [
+                    'Šeimos gydytojas', 
+                    'Akių lygių spec',
+                    'Traumatologas',
+                    'Odontologas'
+                ],
+                'clinic' => [
+                    'Šilainių Poliklinika',
+                    'Dainavos Poliklinika',
+                    'Šančių Poliklinika'
+                ],
+                'city' => ['Kaunas', 'Vilnius', 'Klaipėda'],
+                'doctor' => ['Daktaras', 'Daktarė', 'Seselė']
+        ],
+            'is_logged_in' => false,
+        ]);
+    }
 }
