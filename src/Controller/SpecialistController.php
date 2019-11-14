d9b41fd3a9b890bc495060bfac90bbbb4ccaacf6
Vytas Gavelis <gavelisvytas@gmail.com>
2019-11-14 15:26:56 +0200

    Missing entity added

---
+++ src/Controller/SpecialistController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SpecialistController extends AbstractController
+{
+    /**
+     * @Route("/specialist", name="specialist")
+     */
+    public function index()
+    {
+        return $this->render('specialist/index.html.twig', [
+            'controller_name' => 'SpecialistController',
+        ]);
+    }
+
+    /**
+     * @Route("/specialist/{id}", name="specialist_show")
+     */
+    public function show($id)
+    {
+        $specialist = $this->getDoctrine()->getRepository(User::class)
+            ->findById($id);
+        if(sizeof($specialist) == 0){
+            $specialist = null;
+        }
+        return $this->render('specialist/index.html.twig', [
+            'specialist' => $specialist[0],
+        ]);
+    }
+}
