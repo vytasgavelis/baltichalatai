8fac0d28c1acefd88a0869414094479eabaeb332
Lukas Patackas <xxlas@users.noreply.github.com>
2019-11-19 22:17:40 +0200

    Quick change to search results to include only specialists

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -43,7 +43,7 @@ class UserRepository extends ServiceEntityRepository
             ->where('us.name like :name')
             ->andWhere('us.city like :city')
             ->andWhere('spec.id like :specId')
-            ->andWhere('u.role like %1%')
+            ->andWhere('u.role = 2')
             ->setParameters(['name' => '%'.$name.'%', 'city' => '%'.$city.'%', 'specId' => '%'.$specialty.'%'])
             ->getQuery()
             ->getResult();
