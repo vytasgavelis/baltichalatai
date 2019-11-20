46a906e199844f0d9a76db787194193e632bcd48
Vytas Gavelis <gavelisvytas@gmail.com>
2019-11-20 17:41:39 +0200

    Registration and login working

--- src/Migrations/Version20191120151644.php
+++ src/Migrations/Version20191120151644.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20191117155949 extends AbstractMigration
+final class Version20191120151644 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -23,9 +23,10 @@ final class Version20191117155949 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE clinic_info CHANGE webpage webpage VARCHAR(255) DEFAULT NULL, CHANGE phone_number phone_number VARCHAR(32) DEFAULT NULL, CHANGE email email VARCHAR(255) DEFAULT NULL');
-        $this->addSql('ALTER TABLE user_visit CHANGE is_completed is_completed TINYINT(1) DEFAULT NULL');
+        $this->addSql('ALTER TABLE user ADD email VARCHAR(180) NOT NULL, ADD roles JSON NOT NULL, ADD password VARCHAR(255) NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649E7927C74 ON user (email)');
+        $this->addSql('ALTER TABLE user_visit CHANGE sending_to_doctor_id_id sending_to_doctor_id_id INT DEFAULT NULL, CHANGE recipe_id_id recipe_id_id INT DEFAULT NULL, CHANGE is_completed is_completed TINYINT(1) DEFAULT NULL');
         $this->addSql('ALTER TABLE user_info CHANGE user_id_id user_id_id INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE recipe CHANGE valid_duration valid_duration VARCHAR(255) NOT NULL');
         $this->addSql('ALTER TABLE sending_to_doctor CHANGE description description VARCHAR(2000) DEFAULT NULL');
     }
 
@@ -35,9 +36,10 @@ final class Version20191117155949 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE clinic_info CHANGE webpage webpage VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT \'NULL\' COLLATE `utf8mb4_unicode_ci`, CHANGE phone_number phone_number VARCHAR(32) CHARACTER SET utf8mb4 DEFAULT \'NULL\' COLLATE `utf8mb4_unicode_ci`, CHANGE email email VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT \'NULL\' COLLATE `utf8mb4_unicode_ci`');
-        $this->addSql('ALTER TABLE recipe CHANGE valid_duration valid_duration DATETIME NOT NULL');
         $this->addSql('ALTER TABLE sending_to_doctor CHANGE description description VARCHAR(2000) CHARACTER SET utf8mb4 DEFAULT \'NULL\' COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('DROP INDEX UNIQ_8D93D649E7927C74 ON user');
+        $this->addSql('ALTER TABLE user DROP email, DROP roles, DROP password');
         $this->addSql('ALTER TABLE user_info CHANGE user_id_id user_id_id INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE user_visit CHANGE is_completed is_completed TINYINT(1) DEFAULT \'NULL\'');
+        $this->addSql('ALTER TABLE user_visit CHANGE sending_to_doctor_id_id sending_to_doctor_id_id INT DEFAULT NULL, CHANGE recipe_id_id recipe_id_id INT DEFAULT NULL, CHANGE is_completed is_completed TINYINT(1) DEFAULT \'NULL\'');
     }
 }
