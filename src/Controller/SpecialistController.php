245e7f387780378578187627619b336d3432ea03
Lukas Patackas <xxlas@users.noreply.github.com>
2019-11-24 22:47:50 +0200

    Merge issues fixes

--- src/Controller/SpecialistController.php
+++ src/Controller/SpecialistController.php
@@ -16,6 +16,7 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 class SpecialistController extends AbstractController
