f8fecf39a04742a55e2e19b6e5d181e2ac26ddf0
Diana V <d.valaityte.97@gmail.com>
2019-11-28 08:15:26 +0200

    Styled the registration and log-in page.

--- src/Form/UserInfoType.php
+++ src/Form/UserInfoType.php
@@ -5,7 +5,7 @@ namespace App\Form;
 use App\Entity\UserInfo;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -15,17 +15,16 @@ class UserInfoType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('surname')
-            ->add('phoneNumber')
-            ->add('personalEmail')
-            ->add('dateOfBirth', DateType::class, array(
-                'years' => range(date('Y') - 100, date('Y')),
-            ))
-            ->add('city')
-            ->add('personCode')
-            ->add('description')
-            ->add('save', SubmitType::class);
+            ->add('name', null, ['label'=> 'Vardas'])
+            ->add('surname', null, ['label'=> 'Pavardė'])
+            ->add('phoneNumber', null, ['label'=> 'Telefono Nr.'])
+            ->add('personalEmail', null, ['label'=> 'El. Paštas'])
+            ->add('dateOfBirth', DateType::class, [
+                'years' => range(date('Y') - 100, date('Y')), 'label'=> 'Gimimo Data'])
+            ->add('city', TextType::class, ['label'=> 'Miestas'])
+            ->add('personCode', null, ['label'=> 'Asmens Kodas'])
+            ->add('description', TextType::class, ['label'=> 'Aprašymas'])
+            ->add('save', SubmitType::class,['label'=> 'Išsaugoti']);
     }
 
     public function configureOptions(OptionsResolver $resolver)
