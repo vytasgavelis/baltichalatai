fec3bd3739b2b76ad955884eae5c33c59afa72b3
Vytas <gavelisvytas@gmail.com>
2019-12-11 14:59:05 +0200

    refactored specialist controller authentication

---
+++ src/Services/UserAuthService.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace App\Services;
+
+
+use App\Entity\User;
+
+class UserAuthService
+{
+    /**
+     * @param $user
+     * @return bool
+     */
+    public function isSpecialist($user)
+    {
+        return $user instanceof User && $user->getRole() == 2;
+    }
+
+    /**
+     * @param $user
+     * @return bool
+     */
+    public function isPatient($user)
+    {
+        return $user instanceof User && $user->getRole() == 1;
+    }
+}
